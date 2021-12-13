96aa367e79d193a25ca13136047cb3c233bfa51b
wbytts <wbytts@163.com>
2021-12-14 03:49:37 +0800

    update

---
+++ java8-se-demos/src/test/java/com/by/JacksonDemo01.java
@@ -0,0 +1,29 @@
+package com.by;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class JacksonDemo01 {
+
+    @Test
+    public void test03() {
+        System.out.println("Hello World");
+    }
+
+    @Test
+    public void test02() throws IOException {
+        String jsonFile = "D:/projects/java-codes/java8-se-demos/src/test/java/a.json";
+        File file = new File(jsonFile);
+        System.out.println(file.getAbsolutePath());
+        FileReader reader = new FileReader(new File(jsonFile));
+        char[] buf = new char[1024 * 64];
+        reader.read(buf);
+        System.out.println(new String(buf));
+        reader.close();
+    }
+    
+}
+
---
+++ java8-se-demos/src/test/java/com/by/Test01.java
@@ -0,0 +1,11 @@
+package com.by;
+
+import org.junit.Test;
+
+public class Test01 {
+
+    @Test
+    public void test01() {
+        System.out.println("Hello World");
+    }
+}
