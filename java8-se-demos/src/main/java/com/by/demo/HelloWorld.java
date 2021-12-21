64b5337e68c1895b60e3df75202b5e32bc25ba2e
wangbingyi <1041493283@qq.com>
2021-12-21 17:28:14 +0800

    update

--- java8-se-demos/src/main/java/com/by/demo/HelloWorld.java
+++ java8-se-demos/src/main/java/com/by/demo/HelloWorld.java
@@ -3,6 +3,6 @@ package com.by.demo;
 
 public class HelloWorld {
     public static void main(String[] args)  {
-        System.out.println("Hello World");
+        System.out.println("Hello World你好啊");
     }
 }
