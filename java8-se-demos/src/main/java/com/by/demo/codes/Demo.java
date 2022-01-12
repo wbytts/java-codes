4ebeb10177224a6e3acb496a22a1bcb77adad9e7
wangbingyi <1041493283@qq.com>
2022-01-12 19:45:52 +0800

    update

---
+++ java8-se-demos/src/main/java/com/by/demo/codes/Demo.java
@@ -0,0 +1,27 @@
+package com.by.demo.codes;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) {
+        try {
+            FileInputStream file = new FileInputStream("C:\\Users\\谁将会使用这台电脑？\\Downloads\\粮食配额申请20220111100403.xlsx");
+            XSSFWorkbook wb = new XSSFWorkbook(file);
+            XSSFSheet sheetAt = wb.getSheetAt(0);
+            if(sheetAt == null) {
+                System.out.println("无此标签页");
+                return;
+            }
+            int rowNum = sheetAt.getLastRowNum();
+            int colNum = sheetAt.getRow(0).getLastCellNum();
+            System.out.println("rowNum:" + rowNum + ", colNum:" + colNum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
