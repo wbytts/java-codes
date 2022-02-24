a35540258a4f2f50ffddc438e280d4e7e9fa447e
wangbingyi <1041493283@qq.com>
2022-02-24 10:38:11 +0800

    update

--- java8-se-demos/src/main/java/com/by/demo/P00_test/HelloWorld.java
+++ java8-se-demos/src/main/java/com/by/demo/P00_test/HelloWorld.java
@@ -3,13 +3,14 @@ package com.by.demo.p00_test;
 
 public class HelloWorld {
     public static void main(String[] args)  {
-        String str = """
-            《静夜思》
-            窗前明月光，
-            疑是地上霜。
-            举头望明月，
-            低头思故乡。
-        """;
+        // String str = """
+        //     《静夜思》
+        //     窗前明月光，
+        //     疑是地上霜。
+        //     举头望明月，
+        //     低头思故乡。
+        // """;
+        String str = "123";
         System.out.println(str);
     }
 }
