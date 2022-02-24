159dd2363f8ca715922c7feb9331df125b84ac03
wangbingyi <1041493283@qq.com>
2022-02-24 17:35:57 +0800

    update

--- java8-se-demos/src/main/java/com/by/demo/hutool/HT_0001.java
+++ java8-se-demos/src/main/java/com/by/demo/hutool/HT_0001.java
@@ -1,7 +1,13 @@
 package com.by.demo.hutool;
 
+import cn.hutool.core.util.StrUtil;
+
 public class HT_0001 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(StrUtil.isBlank(""));
+        System.out.println(StrUtil.isBlank("   "));
+        System.out.println(StrUtil.isBlank("\n"));
+        System.out.println(StrUtil.isBlank(null));
+        System.out.println(StrUtil.isBlank("123"));
     }
 }
---
+++ java8-se-demos/src/main/java/com/by/demo/json/gson/Gson_001_hello.java
@@ -0,0 +1,24 @@
+﻿package com.by.demo.json.gson;
+
+import com.google.gson.Gson;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class Gson_001_hello {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Person p = new Person("张三", 18);
+        String jsonStr = gson.toJson(p);
+        System.out.println(jsonStr);
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Person {
+    String name;
+    int age;
+}
