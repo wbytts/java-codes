23ea68fa169552bf902f1cdce2cd810678f1e96b
wbytts <wbytts@163.com>
2022-03-08 00:33:14 +0800

    update

--- java8-se-demos/src/main/java/com/by/demo/basic_grammar/D001_HelloWorld.java
+++ java8-se-demos/src/main/java/com/by/demo/basic_grammar/D001_HelloWorld.java
@@ -1,7 +1,7 @@
 package com.by.demo.basic_grammar;
 
 public class D001_HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
