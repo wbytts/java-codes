08e43a22aea9c0b86e9306441b88591c96333b15
wangbingyi <1041493283@qq.com>
2022-04-15 11:40:13 +0800

    提交

---
+++ java8-se-demos/src/main/java/com/by/temp/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.by.demo.p00_test;
+
+
+public class HelloWorld {
+    public static void main(String[] args)  {
+        // String str = """
+        //     《静夜思》
+        //     窗前明月光，
+        //     疑是地上霜。
+        //     举头望明月，
+        //     低头思故乡。
+        // """;
+        String str = "123";
+        System.out.println(str);
+    }
+}
---
+++ java8-se-demos/src/main/java/com/by/temp/test/RunSysCommandDemo.java
@@ -0,0 +1,28 @@
+package com.by.demo.p00_test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RunSysCommandDemo {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Runtime run = Runtime.getRuntime();
+        Process p = run.exec("ping 127.0.0.1");
+        // Process p = run.exec("chcp");
+        // 读取命令的输出信息
+        InputStream is = p.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        p.waitFor();
+        if (p.exitValue() != 0) {
+            // 说明命令执行失败
+            // 可以进入到错误处理步骤中
+        }
+
+        // 打印输出信息
+        String s = null;
+        while ((s = reader.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
